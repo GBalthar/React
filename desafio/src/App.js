60c15ae4bbb6418fc91ef1d871197138713979da
GBalthar <gbalthar@id.uff.br>
2022-05-31 16:17:32 -0300

    Atualizando Arquivos React

--- desafio/src/App.js
+++ desafio/src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React from 'react';
+import { StylesProvider } from '@material-ui/styles'
+import { CssBaseline } from '@material-ui/core'
+import GlobalStyle from './commons/styles/global-style'
+import Main from './containers/Main'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <StylesProvider injectFirst>
+      <CssBaseline />
+      <GlobalStyle />
+      <Main />
+    </StylesProvider>
   );
 }
 
